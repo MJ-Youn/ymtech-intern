9cc8bb3286d2a4f0a5ded7a8c47023695098803b
MyungJun-Youn <yun0244@naver.com>
2017-01-12 15:49:43 +0900

    Comment 추가

--- InternProject/src/main/java/com/dev2/intern/dao/CommentDAO.java
+++ InternProject/src/main/java/com/dev2/intern/dao/CommentDAO.java
@@ -110,29 +110,68 @@ public class CommentDAO {
 		}
 	}
 	
+	/**
+	 * comment를 추가해주는 함수
+	 * 
+	 * @param writeCommentVO
+	 * 			입력받은 comment의 데이터. parentCommentId, postId, userId, depth, contents를 가지고 있다.
+	 * @return 성공적으로 삽입하면 1, 아니면 0
+	 */
 	public int writeComment(WriteCommentVO writeCommentVO) {
 		return jdbcTemplate.update(QUERY_WRITECOMMENT, writeCommentVO.getParentCommentId(), writeCommentVO.getPostId(), writeCommentVO.getUserId(), writeCommentVO.getDepth(), writeCommentVO.getContents());
 	}
 	
+	/**
+	 * 일반 유저가 comment를 삭제하기 위한 함수 
+	 * 
+	 * @param commentId
+	 * 			삭제하기 위한 comment의 id
+	 * @return 성공적으로 삭제하면 1, 아니면 0
+	 */
 	public int deleteComment(int commentId) {
 		gotoTrash(commentId);
 		
 		return jdbcTemplate.update(QUERY_DELETECOMMENT, commentId);
 	}
 	
+	/**
+	 * 일반 유저가 comment를 삭제할때 데이터를 trash로 이전할때 사용하는 함수
+	 * 
+	 * @param commentId
+	 * 			이전할 comment의 id
+	 */
 	private void gotoTrash(int commentId) {
 		jdbcTemplate.update(QUERY_GOTOTRASH, commentId);
 	}
 	
+	/**
+	 * comment를 수정하기 위한 함수
+	 * 
+	 * @param modifyCommentVO
+	 * 			수정하려고 하는 comment의 데이터. id, content를 가지고 있다.
+	 * @return 성공적으로 수정하면 1, 아니면 0
+	 */
 	public int modifyComment(ModifyCommentVO modifyCommentVO) {
 		return jdbcTemplate.update(QUERY_MODIFYCOMMENT, modifyCommentVO.getContents(), modifyCommentVO.getId());
 	}
 	
+	/**
+	 * 일반 유저가 post 삭제시 post에 달려있는 comment를 같이 삭제하기 위한 함수
+	 * 
+	 * @param postId
+	 * 			삭제하는 post의 id
+	 */
 	public void deleteCommentByPostId(int postId) {
 		gotoTrashByPostId(postId);
 		jdbcTemplate.update(QUERY_DELETECOMMENTBYPOSTID, postId);
 	}
 	
+	/**
+	 * 일반 유저가 comment를 삭제할때 데이터를 trash로 이전할때 사용하는 함수
+	 * 
+	 * @param postId
+	 * 			삭제하는 post의 id
+	 */
 	private void gotoTrashByPostId(int postId) {
 		jdbcTemplate.update(QUERY_GOTOTRASHBYPOSTID, postId);
 	}
